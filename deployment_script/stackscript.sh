dbc9cb829832e260cf75b4741f5a2a3deec93f39
Holden Morris <morrisholdenx@gmail.com>
2023-08-09 08:53:53 -0400

    add noninteractive

--- deployment_script/stackscript.sh
+++ deployment_script/stackscript.sh
@@ -26,6 +26,7 @@ function cleanup {
 
 function setup {
   # install dependancies
+  export DEBIAN_FRONTEND=noninteractive
   apt-get update
   apt-get install -y git python3 python3-pip python3-dev build-essential
 
